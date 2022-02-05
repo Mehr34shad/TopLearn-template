ebb0a0fe9e1b300033c2c7384ed33cb01936094d
Mehrshad.Mod <mehr.m6534@gmail.com>
2022-02-05 16:24:56 +0330

    added header and footer to other pages

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,3 @@
-// var swiper = new Swiper(".swiper", {
-//   spaceBetween: 30,
-//   centeredSlides: true,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
