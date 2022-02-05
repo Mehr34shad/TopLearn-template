10373fbb92b31d99bfc900214c67823a94784bf2
Mehrshad.Mod <mehr.m6534@gmail.com>
2022-02-05 12:44:22 +0330

    Add files to the github repository

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,19 @@
-var swiper = new Swiper(".swiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// var swiper = new Swiper(".swiper", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
 
 $(".owl-carousel").owlCarousel({
   loop: true,
